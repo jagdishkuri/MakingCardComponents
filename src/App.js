6082942da7ecdbee19a060c6128a6c3f3dd6f85c
Jagdish Kuri <jagdishkuri02@gmail.com>
2023-01-29 12:29:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from "./Card";
+function App() {
+  return (
+    <>
+     <Card prop1="Card1">
+      <h3>It is a long sleeve  shirt for men</h3>
+     </Card>
+     </>
+    
+  );
+}
+
+export default App;
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css";
+function Card({prop1,children}){
+    return (
+        <>
+        {prop1=<img src="cloth1.jpeg" alt="none"></img>}
+          {children } 
+        </>
+    )
+
+}
+
+export default Card;
